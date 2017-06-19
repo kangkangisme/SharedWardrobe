d7cb0f4386cd594b7dde4001e359c4330cf54d75
chuan <280335444@qq.com>
2017-06-19 14:44:06 +0800

    Initial commit

---
+++ app/src/main/java/xmut/com/sharedwardrobe/MainActivity.java
@@ -0,0 +1,13 @@
+package xmut.com.sharedwardrobe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
